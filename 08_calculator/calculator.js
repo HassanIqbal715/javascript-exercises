53c27189744cb9e32ed88666881f358e8711c3f9
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-23 14:33:48 +0500

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	var res = 0;
+  for (let i = 0; i < array.length; i++) {
+    res += array[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	var res = array[0];
+  for (let i = 1; i < array.length; i++) {
+    res *= array[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	var res = a;
+  for (let i = 1; i < b; i++) {
+    res *= a;
+  }
+  if (b == 0) {
+    return 1;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	var res = a;
+  for (let i = a - 1; i > 0; i--) {
+    res *= i;
+  }
+  if (a == 0) {
+    return 1;
+  }
+  return res;
 };
 
 // Do not edit below this line
