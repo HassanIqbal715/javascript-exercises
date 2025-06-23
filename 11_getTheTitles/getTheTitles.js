966d0894093eeb463dedacc28bf4e49611792c8f
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-23 14:34:33 +0500

    completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    var titles = [];
+    for (let i = 0; i < books.length; i++) {
+        if ('title' in books[i]) {
+            titles.push(books[i].title);
+        }
+    }
+    return titles;
 };
 
 // Do not edit below this line
