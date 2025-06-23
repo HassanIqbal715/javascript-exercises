a57278b6dd32060e81a88fb9872553fa29cc6620
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-23 14:34:13 +0500

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+function isNumber (a) {
+    return Number.isInteger(Number(a));
+}
 
+const fibonacci = function(n) {
+    if (!isNumber(n)) {
+        n = parseInt(n);
+    }
+    if (n == 0) {
+        return 0;
+    }
+    else if (n <= 0) {
+        return 'OOPS';
+    }
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < n; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
