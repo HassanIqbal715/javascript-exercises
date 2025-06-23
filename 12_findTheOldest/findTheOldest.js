90df88e263d6348fb28cf659ac675867f51df69f
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-06-23 14:41:56 +0500

    Completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let name = '';
+    for (let i = 0; i < people.length; i++) {
+        let age;
+        if ('yearOfDeath' in people[i]) {
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        else {
+            let currentDate = new Date();
+            age = currentDate.getFullYear() - people[i].yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            name = people[i].name;
+        }
+    }
+    return {
+        name,
+        age : oldestAge
+    };
 };
 
 // Do not edit below this line
